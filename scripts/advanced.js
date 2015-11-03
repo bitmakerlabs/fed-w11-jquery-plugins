02d790f4429c8d7d488483474d5a880d425a2c55
betty <makewithbetty@gmail.com>
2015-11-03 17:34:13 -0500

    Resetting files to introductory state

--- scripts/advanced.js
+++ scripts/advanced.js
@@ -1,24 +1,3 @@
 $(document).ready(function(){
-  $('#waypoint-top').waypoint({
-    handler: function(direction) {
-      swal("Oops...", "Something went wrong!", "error");
-    }
-  });
-
-  $('#waypoint-middle').waypoint({
-    offset: "50%",
-    handler: function(direction) {
-      swal("Oops...", "Something went wrong!", "success");
-    }
-  });
-
-  $('#waypoint-bottom').waypoint({
-    offset: "bottom-in-view",
-    handler: function(direction) {
-      if ( direction === "down" ) {
-        swal("Oops...", "Something went wrong!", "info");
-      }
-    }
-  });
 
 });
--- scripts/simple.js
+++ scripts/simple.js
@@ -1,11 +1,3 @@
 $(document).ready(function(){
 
-  $('#btn-super').on('click', function(){
-    swal("Good job!", "You clicked the button!", "success")
-  });
-
-  $('#btn-default').on('click', function(){
-    alert('harro');
-  });
-
 });
